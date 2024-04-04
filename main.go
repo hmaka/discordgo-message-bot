cec275a37ed37a4156b4295e709d9bc34499e5c1
Havish <Havish747@gmail.com>
2024-04-03 22:23:13 -0400

    webserver set up and launched in goroutine

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func launchWebServer(mux *http.ServeMux) {
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// Register the two new handler functions and corresponding URL patterns with
+	// the servemux, in exactly the same way that we did before.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	go launchWebServer(mux)
+	select {}
+}
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from home page\n"))
 }
