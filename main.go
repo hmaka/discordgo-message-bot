83fa3c02377f451e193edf26d57f01379d3d28cb
Havish <Havish747@gmail.com>
2024-04-05 16:13:55 -0400

    Bot functional and streams messages from discord to web front end

--- main.go
+++ main.go
@@ -1,25 +1,99 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
-func launchWebServer(mux *http.ServeMux) {
+func launchWebServer(msg_channel <-chan string) {
+	messages := make([]string, 0)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		home(w, r, &messages)
+	})
+
+	go func(msg_channel <-chan string, messages *[]string) {
+		for msg := range msg_channel {
+			*messages = append(*messages, msg)
+		}
+	}(msg_channel, &messages)
+
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
+	panic(err)
 }
 
 func main() {
 	// Register the two new handler functions and corresponding URL patterns with
 	// the servemux, in exactly the same way that we did before.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
 
-	go launchWebServer(mux)
-	select {}
+	msg_channel := make(chan string)
+	defer close(msg_channel)
+
+	go launchWebServer(msg_channel)
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+	}
+	Token := os.Getenv("DISCORD_TOKEN")
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		messageCreate(s, m, msg_channel)
+	})
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+
 }
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from home page\n"))
+func home(w http.ResponseWriter, r *http.Request, messages *[]string) {
+	w.Write([]byte(strings.Join(*messages, "\n")))
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate, msg_channel chan<- string) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	println(m.Content)
+
+	// code to send message through channel to webserver
+	msg_channel <- m.Content
 }
