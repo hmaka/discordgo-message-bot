03fcfd8665f8aa84dca992136e0533129c7a6d41
Havish <Havish747@gmail.com>
2024-04-08 18:37:41 -0400

    Created pubsub struct to handle multiple web clients

---
+++ pubSub.go
@@ -0,0 +1,38 @@
+package main
+
+type Pubsub struct {
+	Listners *[]chan<- string
+}
+
+func Newpubsub() Pubsub {
+	return Pubsub{
+		Listners: &[]chan<- string{},
+	}
+}
+
+func (p Pubsub) subscribe() <-chan string {
+	ch := make(chan string)
+	*p.Listners = append(*p.Listners, ch)
+
+	return ch
+}
+
+func (p Pubsub) notifyAll(msg string) {
+
+	for _, ch := range *p.Listners {
+		ch <- msg
+	}
+
+}
+
+func (p Pubsub) close() {
+	for _, ch := range *p.Listners {
+		close(ch)
+	}
+}
+
+func threadRun(ch <-chan string) {
+	for msg := range ch {
+		println(msg)
+	}
+}
